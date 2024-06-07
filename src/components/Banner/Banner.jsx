bd447ec5e28ee456c0e757a07fbbb4e3a568aacd
BJoarder <bjoarder87@gmail.com>
2024-06-08 00:15:01 +0600

    banner added

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -1,9 +1,15 @@
-
+import bannerImg from '../../assets/book1.jpg';
 
 const Banner = () => {
   return (
-    <div>
-      
+    <div className="flex justify-center items-center mx-28 my-8 bg-[#1313130D] rounded-lg">
+      <div className="w-3/5 space-y-12 px-12">
+        <h2 className='text-6xl font-bold'>Books to freshen up your bookshelf</h2>
+        <button className='bg-[#23BE0A] px-7 py-5 rounded-lg text-white font-bold text-xl'>View The List</button>
+      </div>
+      <div className="w-2/5 min-h-full">
+        <img src={bannerImg} alt="" />
+      </div>
     </div>
   );
 };
